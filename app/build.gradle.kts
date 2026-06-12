e57409c7b44345465313886a12b9c8ed8949d9f1
beyondallan <beyondallan@outlook.com>
2026-06-12 09:44:17 +0800

    移除flutter，保持android原生

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "tech.ti.social"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "tech.ti.social"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0.0"
 
